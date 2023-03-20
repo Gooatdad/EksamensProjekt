cc4601581919e5bae2a47f2bbb437faddb7769de
Brufman05 <OML2005@outlook.dk>
2023-03-20 10:15:38 +0100

    git commit -m "changes"

--- sketch.js
+++ sketch.js
@@ -1,7 +1,20 @@
+let ball, floor;
+
 function setup() {
-  createCanvas(400, 400);
+	createCanvas(400, 400);
+	world.gravity.y = 10;
+
+	ball = new Sprite();
+	ball.diameter = 50;
+	ball.y = 30;
+
+	floor = new Sprite();
+	floor.y = 190;
+	floor.w = 400;
+	floor.h = 5;
+	floor.collider = 'static';
 }
 
 function draw() {
-  background(220);
+	clear();
 }
