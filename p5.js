deee48fb6e03d2e64ac6636b7c327eadc7a87138
Brufman05 <OML2005@outlook.dk>
2023-03-20 09:33:09 +0100

    goofy

--- p5.js
+++ p5.js
@@ -1,20 +1,6 @@
-let ball, floor;
-
 function setup() {
-	new Canvas(238, 200);
-	world.gravity.y = 10;
-
-	ball = new Sprite();
-	ball.diameter = 50;
-	ball.y = 30;
-
-	floor = new Sprite();
-	floor.y = 190;
-	floor.w = 238;
-	floor.h = 5;
-	floor.collider = 'static';
+	createCanvas(400, 400);
 }
-
 function draw() {
-	clear();
+	background(100);
 }
