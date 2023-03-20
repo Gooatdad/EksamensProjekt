86a31f1bad6181cc2feffd2ffda30b96fb17016e
Brufman05 <OML2005@outlook.dk>
2023-03-20 10:53:07 +0100

    Goofy platform

--- sketch.js
+++ sketch.js
@@ -1,19 +1,20 @@
 let ball, floor;
 
 function setup() {
-	createCanvas(400, 400);
+	createCanvas(1960, 1080);
 	world.gravity.y = 10;
 
 	ball = new Sprite();
 	ball.diameter = 50;
 	ball.y = 30;
+  ball.x = 100;
 
 	floor = new Sprite();
 	floor.y = 190;
-	floor.w = 400;
+	floor.w = 1960;
 	floor.h = 5;
 	floor.collider = 'static';
-  //Test test
+  floor.rotation = 2;
 }
 
 function draw() {
