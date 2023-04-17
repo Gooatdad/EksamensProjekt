4e84da07f2ea35503221ed80bb5165a828573247
Brufman05 <OML2005@outlook.dk>
2023-04-17 08:45:35 +0200

    Bold der falder

--- sketch.js
+++ sketch.js
@@ -1,37 +1,26 @@
-let = PlayerX = 30;
-let = PlayerY = 30;	
-let = TAcc = 0.5;
-let = TAccM = 0.2;
-let = RGBJ = (0, 255, 0);
+let ball;
+let floor;
 
-function setup(){
-createCanvas(innerWidth, innerHeight);
+function setup() {
+	new Canvas(windowWidth, windowHeight);
+	world.gravity.y = 10;
 
+	ball = new Sprite();
+	ball.diameter = 50;
+	ball.y = 30;
 
-	
+	floor = new Sprite();
+	floor.y = 190;
+	floor.x = 100;
+	floor.w = 200;
+	floor.h = 5;
+	floor.collider = 'static';
 }
 
-function draw(){
-	background(100);
+function draw() {
+	background(0);
 
-	fill(51);
-	rect(0, 700, innerWidth, 200);
-	
-	rect(PlayerX, PlayerY, 50, 80);
-	
-	if (keyIsDown(LEFT_ARROW)){
-		PlayerX -= 2;
-	}
-	if (keyIsDown(RIGHT_ARROW)){
-		PlayerX += 2;
-	}
-	//Tyngekraft
-	PlayerY += TAcc;
-	TAcc += TAccM;
-
-	if (PlayerY > 700){
-		PlayerY == 700;
-		TAcc == 0;
-	}
-
+	ball.draw();
+	floor.draw();
 }
+
